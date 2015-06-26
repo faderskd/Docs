05c7cfd7d86d3294f0967c4e28fc364ad972ae06
faderskd <daniel.faderski@gmail.com>
2015-06-26 15:16:23 +0200

    cos tam cos tam

--- api/models.py
+++ api/models.py
@@ -18,6 +18,9 @@ class Author(models.Model):
     salutation = models.CharField(max_length=10)
     name = models.CharField(max_length=200)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):              # __unicode__ on Python 2
         return self.name
 
