644f1b49215cf64294f54eb8c553efdf0ba7e501
faderskd <daniel.faderski@gmail.com>
2015-06-27 15:22:51 +0200

    branch branch

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class Publisher(models.Model):
     class Meta:
         ordering = ["-name"]
 
-zmiana poczatkowa na serwerze (nowa)
+zmiana poczatkowa na serwerze
 
     def __str__(self):              # __unicode__ on Python 2
         return self.name
