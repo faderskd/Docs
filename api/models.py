609a2724da701c9a37d69c82665a0379317fd28c
faderskd <daniel.faderski@gmail.com>
2015-06-27 13:46:50 +0200

    branch branch

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class Publisher(models.Model):
     class Meta:
         ordering = ["-name"]
 
-i ktos tez zmienil
+ja teraz zmienilem
 
     def __str__(self):              # __unicode__ on Python 2
         return self.name
