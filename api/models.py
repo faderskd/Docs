91d3e75fd093b017723cd3820b6272e28ee121b8
faderskd <daniel.faderski@gmail.com>
2015-06-27 17:47:50 +0200

    branch branch

--- api/models.py
+++ api/models.py
@@ -24,7 +24,7 @@ class Author(models.Model):
 
     class Meta:
         ordering = ['salutation']
-nowy tekst
+
 
     def __str__(self):              # __unicode__ on Python 2
         return self.name
