a0e0152f099bdef1868335d51d9b41adb56c9ff2
faderskd <daniel.faderski@gmail.com>
2015-06-27 18:20:11 +0200

    Update models.py

--- api/models.py
+++ api/models.py
@@ -24,7 +24,7 @@ class Author(models.Model):
 
     class Meta:
         ordering = ['salutation']
-
+zmiana serwer
 
     def __str__(self):              # __unicode__ on Python 2
         return self.name
