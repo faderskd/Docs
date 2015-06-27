36cf03e61ef229bcabc8c76e3340e80a75bf3564
faderskd <daniel.faderski@gmail.com>
2015-06-27 15:24:32 +0200

    Update models.py

--- api/models.py
+++ api/models.py
@@ -11,7 +11,7 @@ class Publisher(models.Model):
     class Meta:
         ordering = ["-name"]
 
-zmiana poczatkowa na serwerze (najnowsza)
+zmiana poczatkowa na serwerze (najnajnowsza)
 
     def __str__(self):              # __unicode__ on Python 2
         return self.name
